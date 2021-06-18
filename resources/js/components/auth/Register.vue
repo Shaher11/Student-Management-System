5a71f319c6534f4f2fe722196a25f4fca0a4c99c
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-06-18 22:04:58 +0200

    Components | Login and forget password

---
+++ resources/js/components/auth/Register.vue
@@ -0,0 +1,18 @@
+<template>
+        <h1> Register Form </h1>
+
+</template>
+
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
---
+++ resources/js/routes.js
@@ -0,0 +1,16 @@
+
+
+let Login = require('./components/auth/Login.vue').default;
+// let Register = require("./components/auth/Register.vue").default;
+let Forget = require("./components/auth/Forget.vue").default;
+// let logout = require("./components/auth/logout.vue").default;
+
+
+
+
+
+export const routes = [
+           { path: "/", component: Login, name: "/" },
+           //    { path: "/register", component: Register, name: "register" }
+           { path: "/forget", component: Forget, name: "forget" }
+       ];
