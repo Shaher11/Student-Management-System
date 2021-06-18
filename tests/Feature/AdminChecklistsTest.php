0902e6784b225cd9a616607e71c9d0788a9e8447
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-06-18 18:36:03 +0200

    Insyall Vue.js Framework

--- tests/Feature/AdminChecklistsTest.php
+++ tests/Feature/AdminChecklistsTest.php
@@ -3,12 +3,14 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class AdminChecklistsTest extends TestCase
 {
+    use RefreshDatabase;
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
