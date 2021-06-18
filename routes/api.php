f2b56d99bef2c54c202b9aef12fa9c0c8403e44b
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-06-18 23:42:39 +0200

    Front-end Auth using JWT | create helper folder

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::group([
     Route::post('logout', [AuthController::class,'logout']);
     Route::post('refresh', [AuthController::class,'refresh']);
     Route::post('me', [AuthController::class, 'me']);
-    Route::post('register', [AuthController::class, 'register']);
+    // Route::post('register', [AuthController::class, 'register']);
 
 });
 
